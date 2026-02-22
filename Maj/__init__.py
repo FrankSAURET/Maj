398302776d28a31b3f6f5d739c5cebfcbabfa6ce
frank4466 <frank.sauret@outlook.fr>
2026-02-22 17:41:13 +0100

    2026.2

--- Maj/__init__.py
+++ Maj/__init__.py
@@ -1,3 +1,15 @@
-__version__ = "2026.1"
-__author__ = "Frank SAURET & GitHub Copilot"
+
+import json
+import os
+
+info_path = os.path.join(os.path.dirname(__file__), 'Info.json')
+try:
+	with open(info_path, 'r', encoding='utf-8') as f:
+		info = json.load(f)
+	__version__ = info.get('version', 'inconnue')
+	__author__ = info.get('author', 'inconnu')
+except Exception:
+	__version__ = 'inconnue'
+	__author__ = 'inconnu'
+
 __license__ = "GPLv2"
